c313eef576e85cc9c9e4eb02edf6329b63a4f3a9
Maksim Lysak <maxim.hasp1@gmail.com>
2022-07-13 16:20:25 +0300

    template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import './styles.css'
+import './styles.css';
+
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+const counter = document.getElementById('counter');
+
+let state = 10;
+
+const render = () => {
+	counter.textContent = state.toString();
+};
+
+addBtn.addEventListener('click', () => {
+	state++;
+	render();
+});
+
+subBtn.addEventListener('click', () => {
+	state--;
+	render();
+});
+
+themeBtn.addEventListener('click', () => {
+	document.body.classList.toggle('dark');
+});
+
+asyncBtn.addEventListener('click', () => {
+	setTimeout(() => {
+		state++;
+		render();
+	}, 2000);
+});
+
+render();
